19bad583a890f15d360a4a1f5668be021dd9d9dd
Bruno Rodrigues <brunodasilvarodrigues.28@gmail.com>
2022-10-23 17:22:35 -0300

    Projeto Final

--- app/src/main/java/com/example/gasto_viagem/MainActivity.kt
+++ app/src/main/java/com/example/gasto_viagem/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.gasto_viagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.system.Os
+import android.view.View
+import android.widget.Toast
+import com.example.gasto_viagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCalcular.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calcular){
+            calcular()
+        }
+    }
+
+    private fun validacao() : Boolean{
+        return (binding.editDistancia.text.toString() != ""
+                && binding.editPreco.text.toString() != ""
+                && binding.editAutonomia.text.toString() != "")
+    }
+
+    private fun calcular(){
+        if (validacao()) {
+            if (binding.editAutonomia.text.toString().toFloat() != 0f) {
+                val distancia = binding.editDistancia.text.toString().toFloat()
+                val preco = binding.editPreco.text.toString().toFloat()
+                val autonomia = binding.editAutonomia.text.toString().toFloat()
+
+                val valorTotal = (distancia * preco) / autonomia
+                binding.textValorTotal.text = "R$ ${"%.2f".format(valorTotal)}"
+            } else {
+                Toast.makeText(this, R.string.valorzero, Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this, R.string.validacao, Toast.LENGTH_SHORT).show()
+        }
     }
 }
